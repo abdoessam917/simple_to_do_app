58cb08fb7d2de2a4b70a0841443c9d1a7876657b
abdo-essam <abdo.e.mosa@gmail.com>
2025-05-22 15:10:20 +0300

    Initial commit

---
+++ lib/screens/task_list_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+import '../model/task.dart';
+
+class TaskListScreen extends StatefulWidget {
+  const TaskListScreen({super.key});
+
+  @override
+  State<TaskListScreen> createState() => _TaskListScreenState();
+}
+
+class _TaskListScreenState extends State<TaskListScreen> {
+  TextEditingController textController = TextEditingController();
+  List<Task> tasks = [
+    Task(title: "Learn Flutter Basics"),
+    Task(title: "Build a Simple App"),
+    Task(title: "Publish to GitHub"),
+    Task(title: "Share with friends"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Tasks')),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: textController,
+                    decoration: InputDecoration(
+                      hintText: 'Add a new task',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                     if (textController.text.isNotEmpty) {
+                        tasks.add(Task(title: textController.text));
+                        textController.clear();
+                      }
+                    });
+                  },
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child:
+                tasks.isEmpty
+                    ? Center(child: Text("No tasks available"))
+                    : ListView.builder(
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(tasks[index].title),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () {
+                              setState(() {
+                                tasks.removeAt(index);
+                              });
+                            },
+                          ),
+                        );
+                      },
+                      itemCount: tasks.length,
+                    ),
+          ),
+        ],
+      ),
+    );
+  }
+}
